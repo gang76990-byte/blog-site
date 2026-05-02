ef2b22efbbc490fb3544f70b64334c03df405713
javen <2856572867@qq.com>
2026-05-03 01:43:02 +0800

    Deploy static site (2026-05-03 01:43:02)

---
+++ assets/notes.md.uoMI-hS2.js
@@ -0,0 +1,14 @@
+import { _ as _export_sfc, s as resolveComponent, o as openBlock, c as createElementBlock, a as createVNode } from "./chunks/framework.KpO8PD4X.js";
+const __pageData = JSON.parse('{"title":"笔记","description":"","frontmatter":{"title":"笔记","kicker":"任何东西都有时效性，理性看待。","pageClass":"notes-page"},"headers":[],"relativePath":"notes.md","filePath":"notes.md"}');
+const _sfc_main = { name: "notes.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_NotesPage = resolveComponent("NotesPage");
+  return openBlock(), createElementBlock("div", null, [
+    createVNode(_component_NotesPage)
+  ]);
+}
+const notes = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  notes as default
+};
